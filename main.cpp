e69bd88d34dc2b24fac0df70cb427a7f9d5cb824
GyMatyi <gyimesimatyas@gmail.com>
2024-10-22 22:51:47 +0200

    hibak javitva

--- main.cpp
+++ main.cpp
@@ -4,25 +4,32 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int b = new int[NELEMENTS];  // N_ELEMENTS név helyett NELEMENTS volt használva, ami nem létezik.
-    std::cout << '1-100 ertekek duplazasa';
-    for (int i = 0;)  // A for ciklusból hiányzik a feltétel és az inkrementálás.
-    {
-        b[i] = i 2;
-    }
-    for (int i = 0; i; i++)  // A ciklus feltétele mindig hamis lesz, mert i kezdetben 0, így a ciklus nem fut le.
-    {
-        std::cout << "Ertek:";
-    }
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
 
-    for (int i = 0; i < N_ELEMENTS, i++)  // (,) helyett (;) kellene használni a for feltételében.
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        b[i] = (i + 1) 2;
+    }
+
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        std::cout << "Ertek: " << b[i] << std::endl;
+    }
+
+    std::cout << "Atlag szamitasa: " << std::endl;
+
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         atlag += b[i];
     }
 
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0; //memory leak, nincs torolve a b ponter
+
+    std::cout << "teszt collab" << std::endl;
+
+    delete[] b;
+    return 0;
 }
