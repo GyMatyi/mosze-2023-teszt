3288379ed09b1b73d95cc0ef7180b447ab564110
GyMatyi <gyimesimatyas@gmail.com>
2024-10-22 22:50:01 +0200

    hibak commentezve

--- main.cpp
+++ main.cpp
@@ -4,23 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int b = new int[NELEMENTS];  // N_ELEMENTS név helyett NELEMENTS volt használva, ami nem létezik.
+    std::cout << '1-100 ertekek duplazasa';
+    for (int i = 0;)  // A for ciklusból hiányzik a feltétel és az inkrementálás.
     {
-        b[i] = i * 2;
+        b[i] = i 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)  // A ciklus feltétele mindig hamis lesz, mert i kezdetben 0, így a ciklus nem fut le.
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:";
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+
+    for (int i = 0; i < N_ELEMENTS, i++)  // (,) helyett (;) kellene használni a for feltételében.
     {
-        atlag += b[i]
+        atlag += b[i];
     }
+
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+    return 0; //memory leak, nincs torolve a b ponter
 }
