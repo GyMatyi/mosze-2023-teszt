fdb1322f8ad376e449c5beeb1cce72cdbf2c5778
GyMatyi <gyimesimatyas@gmail.com>
2024-11-14 21:30:08 +0100

    vegleges jo fajl

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int b = new int[N_ELEMENTS];
+    int *b = new int[N_ELEMENTS];
+
     std::cout << "1-100 ertekek duplazasa" << std::endl;
+    std::cout << "-------------" << std::endl;
 
     for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = (i + 1) 2;
+        b[i] = (i + 1)*2;
     }
 
     for (int i = 0; i < N_ELEMENTS; i++)
     {
         std::cout << "Ertek: " << b[i] << std::endl;
     }
-
-    std::cout << "Atlag szamitasa: " << std::endl;
 
     int atlag = 0;
     for (int i = 0; i < N_ELEMENTS; i++)
@@ -26,9 +26,10 @@ int main()
     }
 
     atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
 
-    std::cout << "teszter collab" << std::endl;
+    std::cout << "-------------" << std::endl;
+    std::cout << "Atlag: " << atlag << std::endl;
     delete[] b;
     return 0;
 }
+
